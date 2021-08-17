671a45a5a9c86429b579e9fddfcfd0efe4c32c26
Justyboy <41842024+Justyboy@users.noreply.github.com>
2021-08-17 13:50:18 -0400

    ADD: Detail page buttons done

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,23 +5,25 @@ function Detail() {
   return (
     <Container>
       <Background>
-      <img src="/images/viewers-disney.png" />
+      <img src="\images\jayme-mccolgan-IP1rSwr8R6o-unsplash.jpg" />
       </Background>
       <ImageTitle>
       <img src="/images/viewers-disney.png" />
       </ImageTitle>
       <Controls>
-      <Playbutton>
-
-      </Playbutton>
+      <PlayButton>
+      <img src="/images/play-icon-black.png" />
+      <span>PLAY</span>
+      </PlayButton>
       <TrailerButton>
-
+      <img src="/images/play-icon-white.png" />
+      <span>TRAILER</span>
       </TrailerButton>
       <AddButton>
-
+      <span>+</span>
       </AddButton>
       <GroupWatchButton>
-
+      <img src="/images/group-icon.png" />
       </GroupWatchButton>
       </Controls>
     </Container>
@@ -34,6 +36,8 @@ const Container = styled.div`
 min-height: calc(100vh - 70px); 
 padding: 0 calc(3.5vw + 5px); 
 position: relative;
+
+
 `
 
 const Background = styled.div`
@@ -42,6 +46,7 @@ const Background = styled.div`
  left: 0;
  bottom: 0;
   right: 0;
+  z-index: -1;
 
   img{
     width: 100%;
@@ -63,14 +68,50 @@ img{
 `
 
 const Controls = styled.div`
-
+display: flex;
+align-items: center;
 `
 
-const Playbutton = styled.div`
+const PlayButton = styled.button`
+border-radius: 4px;
+font-size: 15px;
+display: flex;
+align-items: center;
+height: 56px;
+background: rgb(249,249,249);
+border:none;
+padding: 0 24px;
+margin-right: 22px;
+letter-spacing: 1.8px;
+cursor: pointer;
+
+&:hover{
+  background: rgb(198,198,198);
+}
 `
-const TrailerButton = styled.div`
+const TrailerButton = styled(PlayButton)`
+background: rgba(0,0,0,0.3);
+border: 1px solid rgb(249,249,249);
+color: rgb(249, 249, 249); 
+text-transform: uppercase;
 `
-const AddButton = styled.div`
+const AddButton = styled.button`
+width: 44px;
+height: 44px;
+display:flex;
+align-items: center;
+justify-content: center;
+border-radius: 50%;
+border: 2px solid white;
+background-color: rgba(0,0,0, 0.6);
+cursor: pointer;
+margin-right: 16px;
+
+span{
+  font-size: 30px;
+  color: #fff;
+}
 `
-const GroupWatchButton = styled.div`
+const GroupWatchButton = styled(AddButton)`
+background-color: rgb(0,0,0);
 `
